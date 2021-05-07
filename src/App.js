509ebc144eb96f00e32362058ae52f001734b16e
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-07 14:47:38 -0400

    Response to exercises 1.1 & 1.2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from "react";
 
-const Hello = (props) => {
+const Header = (props) => {
   return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old </p>
-    </div>
+    <h1>
+      {props.course}
+    </h1>
   )
 }
 
-
-const App = () => {
-  const name = 'Iktan'
-  const age = 18
-
+const Content = (props) => {
   return (
     <div>
-      <h1>Greetings!</h1>
-      <Hello name={"Han"} age={-26 + -1} />
-      <Hello name={"Inés"} age={12 / 2} />
-      <Hello name={name} age={age} />
+      <p>{props.part} has a total of {props.exercises} exercises</p>
     </div>
   );
 };
+
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        The total number of exercises is {props.total}
+      </p>
+    </div>
+  );
+};
+
+
+const App = () => {
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+  const part3 = "State of a component"
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  );
+};
+
+
 
 export default App;
