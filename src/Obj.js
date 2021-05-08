6a0aa12826e0b34508891bb532194290dc8af402
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-08 11:05:05 -0400

    render first component

--- src/Obj.js
+++ src/Obj.js
@@ -1,13 +1,43 @@
 import React from "react";
 
+const Title = ({ event }) => <h1>{event}</h1>
 
+const Obj = () => {
+  const developers = {
+    event: "Resilient Coders Community Hours",
+    members: [
+      {
+        firstName: "Sofia",
+        lastName: "I.",
+        country: "Argentina",
+        continent: "Americas",
+        age: 35,
+        language: "Java",
+      },
+      {
+        firstName: "Lukas",
+        lastName: "X.",
+        country: "Croatia",
+        continent: "Europe",
+        age: 35,
+        language: "Python",
+      },
+      {
+        firstName: "Madison",
+        lastName: "U.",
+        country: "United States",
+        continent: "Americas",
+        age: 32,
+        language: "Ruby",
+      },
+    ],
+  };
 
-const Obj = () =>{
-    return (
-        <div>
-            <p>Hey!</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Title event={developers.event} />
+    </div>
+  );
+};
 
 export default Obj;
