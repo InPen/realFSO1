3fa4871bd0780aa64ece8b11a4a1eba4166705f2
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-10 13:14:09 -0400

    Include useState hook

--- src/UniCafe.js
+++ src/UniCafe.js
@@ -1,12 +1,15 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const UniCafe = () => {
-  
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+
   return (
     <div>
-      <h1>Hellooo</h1>
-
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
     </div>
   );
 };
