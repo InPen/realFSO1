0357b56b083b4e162f687f8db9d0c5c8c2beb428
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-14 12:40:53 -0400

    refactor code so counter function lives in return statement

--- src/Counter.js
+++ src/Counter.js
@@ -6,16 +6,11 @@ const Counter = (props) => {
   //The function call adds state to the component and renders it initialized with the value of zero.
   //The counter variable is assigned the initial value of state which is zero. The variable setCounter is assigned to a function that will be used to modify the state.
   const [counter, setCounter] = useState(0);
-  const handleClick = () =>{
-      console.log('clicked');
-  }
-
-  setTimeout(() => setCounter(counter + 1), 1000);
   
   return( 
     <div>
       {counter}
-      <button onClick={handleClick}>Plus</button>
+      <button onClick={() => setCounter(counter + 1)}>Plus</button>
     </div>
 )};
 
