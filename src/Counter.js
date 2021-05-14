61f3692c12902d57d4dd986ca116427a10293f78
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-14 12:37:44 -0400

    Implement event handler onClick

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,22 @@
+//imports the useState function
 import React, { useState } from "react";
 
 const Counter = (props) => {
-  const [ counter, setCounter ] = useState(0)
-  setTimeout(
-      () => setCounter(counter + 1),
-      1000
-  )
-  return <div>{counter}</div>;
-};
+  //function body that defines the component
+  //The function call adds state to the component and renders it initialized with the value of zero.
+  //The counter variable is assigned the initial value of state which is zero. The variable setCounter is assigned to a function that will be used to modify the state.
+  const [counter, setCounter] = useState(0);
+  const handleClick = () =>{
+      console.log('clicked');
+  }
+
+  setTimeout(() => setCounter(counter + 1), 1000);
+  
+  return( 
+    <div>
+      {counter}
+      <button onClick={handleClick}>Plus</button>
+    </div>
+)};
 
 export default Counter;
