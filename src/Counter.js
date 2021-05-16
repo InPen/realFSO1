cf360134da8cac89bba18feefb0df5284598aa53
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-16 12:55:10 -0400

    Include explanation at bottom of file

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,9 @@
 //imports the useState function
 import React, { useState } from "react";
 
-const Display = (props) => {
+const Display = ({ counter }) => {
   return (
-    <div>{props.counter}</div>
+    <div>{counter}</div>
   )
 }
 
@@ -36,3 +36,10 @@ const Counter = (props) => {
   );};
 
 export default Counter;
+
+
+// When the application starts, the code in App is executed. This code uses a useState hook to create the application state, setting an initial value of the variable counter. This component contains the Display component - which displays the counter's value, 0 - and three Button components. The buttons all have event handlers, which are used to change the state of the counter.
+
+// When one of the buttons is clicked, the event handler is executed. The event handler changes the state of the App component with the setCounter function. Calling a function which changes the state causes the component to rerender.
+
+// So, if a user clicks the plus button, the button's event handler changes the value of counter to 1, and the App component is rerendered. This causes its subcomponents Display and Button to also be re-rendered. Display receives the new value of the counter, 1, as props. The Button components receive event handlers which can be used to change the state of the counter.
