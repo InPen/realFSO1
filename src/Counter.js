5a6c176f58ecd2aa84428723f4414527e812aa4a
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-16 12:49:35 -0400

    Instead of rendering the function in our return we are creating indivial functions above and invoking them in our return statement

--- src/Counter.js
+++ src/Counter.js
@@ -7,17 +7,31 @@ const Display = (props) => {
   )
 }
 
+const Button = ({ text }) => {
+
+  return (
+    <button onClick = {text.handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const Counter = (props) => {
   //function body that defines the component
   //The function call adds state to the component and renders it initialized with the value of zero.
   //The counter variable is assigned the initial value of state which is zero. The variable setCounter is assigned to a function that will be used to modify the state.
   const [counter, setCounter] = useState(0);
 
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+
   return (
     <div>
-      <Display counter = {counter} />
-      <button onClick={() => setCounter(counter + 1)}>Plus</button>
-      <button onClick={() => setCounter(0)}>Zero</button>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
     </div>
   );};
 
