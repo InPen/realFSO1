525fae484ecb48aa0d463a72f4dfb2a228cb9454
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-16 10:25:01 -0400

    create a new component to display counter separetly

--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,21 @@
 //imports the useState function
 import React, { useState } from "react";
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
 const Counter = (props) => {
   //function body that defines the component
   //The function call adds state to the component and renders it initialized with the value of zero.
   //The counter variable is assigned the initial value of state which is zero. The variable setCounter is assigned to a function that will be used to modify the state.
   const [counter, setCounter] = useState(0);
-  
+
   return (
     <div>
-      {counter}
+      <Display counter = {counter} />
       <button onClick={() => setCounter(counter + 1)}>Plus</button>
       <button onClick={() => setCounter(0)}>Zero</button>
     </div>
