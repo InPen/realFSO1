f098a8f23dc85af7631b183e798acbbb889e508b
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-20 10:39:15 -0400

    Create Button component and check if its functional by implementing console.logs

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 
+const Button = ({handleClick, text}) => {
+    return(
+        <button onClick={handleClick}>
+            {text}
+        </button>
+    )
+}
+
 const Feedback = () => {
     return (
-        <div>
-            <h1>Give Feedback</h1>
-        </div>
-    )
+      <div>
+        <h1>Give Feedback</h1>
+        <Button handleClick={() => console.log("clicked")} text="Good" />
+        <Button handleClick={() => console.log("clicked")} text="Neutral" />
+        <Button handleClick={() => console.log("clicked")} text="Bad" />
+      </div>
+    );
 }
 
 export default Feedback;
