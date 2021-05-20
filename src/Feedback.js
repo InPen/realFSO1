50bd18f20b7bbe96fbb1d06c8b11b116f6e7c993
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-20 11:04:34 -0400

    Include useState

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-
-const Button = ({handleClick, text}) => {
-    return(
-        <button onClick={handleClick}>
-            {text}
-        </button>
-    )
-}
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
 
 const Feedback = () => {
-    return (
-      <div>
-        <h1>Give Feedback</h1>
-        <Button handleClick={() => console.log("clicked")} text="Good" />
-        <Button handleClick={() => console.log("clicked")} text="Neutral" />
-        <Button handleClick={() => console.log("clicked")} text="Bad" />
-      </div>
-    );
-}
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={() => console.log("clicked")} text="Good" />
+      <Button handleClick={() => console.log("clicked")} text="Neutral" />
+      <Button handleClick={() => console.log("clicked")} text="Bad" />
+    </div>
+  );
+};
 
 export default Feedback;
