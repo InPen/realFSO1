4a17dcf8fda9e073b0442be6558d597df655f55a
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-05-21 11:22:41 -0400

    Include functions to calculate total,average, and positive feedback

--- src/Feedback.js
+++ src/Feedback.js
@@ -4,11 +4,20 @@ const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
+
+
+
+
 const Feedback = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+
+  const total = good + neutral + bad
+  const average = total / 3
+  const positive = good + neutral 
 
   return (
     <div>
@@ -21,6 +30,9 @@ const Feedback = () => {
       <p>Good : {good}</p>
       <p>Neutral : {neutral}</p>
       <p>Bad : {bad}</p>
+      <p>Total : {total}</p>
+      <p>Average</p>
+      <p>Positive</p>
     </div>
   );
 };
